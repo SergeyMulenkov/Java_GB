729281513cfc9488ede28494edbdb2e9e6e29200
Sergey Mulenkov <sergej.mulenkov@gmail.com>
2023-03-05 17:53:30 +0300

    1 семинар

---
+++ src/main/java/org/example/hw/hw1/Ex1.java
@@ -0,0 +1,28 @@
+package org.example.hw.hw1;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] part = str.trim().split("\\s+");
+        print(part);
+        array(part);
+        System.out.println();
+        print(part);
+    }
+    public static void print(String [] arr) { //метод печати
+        for (String i : arr) {
+            System.out.printf((i) + " ");
+        }
+    }
+    public static String[] array (String [] arr){ //метод замены
+        for (int i = 0; i < arr.length / 2; i++) {
+            String temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+        return arr;
+    }
+}
